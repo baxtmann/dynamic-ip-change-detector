c7bd9e029e666510b667b37d22287e9044942431
Brandon Axtmann <baxtmann@hotmail.ca>
2022-08-26 20:00:46 -0400

    change Requests import and add req headers
    - Change way we import headers lib
    - Add headers to request (CF workers Requirement)
    - Redo request to match new import

--- dynamic-ip-change-detector.py
+++ dynamic-ip-change-detector.py
@@ -1,8 +1,8 @@
-import urllib.request
 import time
 import json
 import os
 from discord_webhook import DiscordWebhook, DiscordEmbed
+from urllib.request import Request, urlopen
 
 discord_webhook = os.environ['DISCORD_WEBHOOK']
 
@@ -19,7 +19,14 @@ currentip = []
 
 while True:
     endpoint = 'https://ip.edge9.workers.dev/' # Do not change this variable unless you know what you're doing
-    data = urllib.request.urlopen(endpoint).read()
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+    request = Request(endpoint, headers=hdr)
+    data = urlopen(request).read()
     ip = data.decode().strip()
 
     print(ip)
